20b4775db34ba5c61d18ab9a4c4511df0ecd3c3c
Starbinder <Starbinder99@gmail.com>
2022-05-11 22:36:05 +0800

    not done

--- student_feedback.php
+++ student_feedback.php
@@ -1,251 +1,207 @@
-<?php
-include'process.php';
-$q=mysqli_query($conn,"SELECT * FROM student where STUDENT_ID = $userID");
- 				
- 				$row=mysqli_fetch_assoc($q);
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
 <head>
-    
-    <title>Student Feedback</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/style.css">
-		<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
-		<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
-		<link rel='stylesheet' href='https://raw.githubusercontent.com/kartik-v/bootstrap-star-rating/master/css/star-rating.min.css'>
-	    <style type="text/css">
-
-
-
-    	body
-    	{
-    		background-image: url("images/66.jpg");
-    		background-repeat: no-repeat;
-    	}
-    	.wrapper
-    	{
-    		padding: 10px;
-    		margin: -20px auto;
-    		width:900px;
-    		height: 600px;
-    		background-color: black;
-    		opacity: .8;
-    		color: white;
-    	}
-    	.form-control
-    	{
-    		height: 100px;
-    		width: 100%;
-    	}
-    	.scroll
-    	{
-    		width: 50%;
-    		height: 100px;
-    		overflow: auto;
-    	
-    	}
-
-   
-    </style>
+    <meta charset="utf-8" />
+    <title>Feedback Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="container">
+    	<h1 class="mt-5 mb-5">Feedback Page</h1>
+    	<div class="card">
+    		<div class="card-header">Comapany Name</div>
+    		<div class="card-body">
+    			<div class="row">
+    				<div class="col-sm-4 text-center">
+    					<h1 class="text-warning mt-4 mb-4">
+    						<b><span id="average_rating">0.0</span> / 5</b>
+    					</h1>
+    					<div class="mb-3">
+    						<i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+                            <i class="fas fa-star star-light mr-1 main_star"></i>
+	    				</div>
+    					<h3><span id="total_review">0</span> Review</h3>
+    				</div>
+    				<div class="col-sm-4">
+    					<p>
+                            <div class="progress-label-left"><b>5</b> <i class="fas fa-star text-warning"></i></div>
 
-
-<header class="ScriptHeader">
-    <div class="rt-container">
-    	<div class="col-rt-12">
-        	<div class="rt-heading">
-            	<h1>Student Feedback</h1>
-                <p>Please give us your feedback</p>
-            </div>
-        </div>
+                            <div class="progress-label-right">(<span id="total_five_star_review">0</span>)</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="five_star_progress"></div>
+                            </div>
+                        </p>
+    					<p>
+                            <div class="progress-label-left"><b>4</b> <i class="fas fa-star text-warning"></i></div>
+                            
+                            <div class="progress-label-right">(<span id="total_four_star_review">0</span>)</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="four_star_progress"></div>
+                            </div>               
+                        </p>
+    					<p>
+                            <div class="progress-label-left"><b>3</b> <i class="fas fa-star text-warning"></i></div>
+                            
+                            <div class="progress-label-right">(<span id="total_three_star_review">0</span>)</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="three_star_progress"></div>
+                            </div>               
+                        </p>
+    					<p>
+                            <div class="progress-label-left"><b>2</b> <i class="fas fa-star text-warning"></i></div>
+                            
+                            <div class="progress-label-right">(<span id="total_two_star_review">0</span>)</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="two_star_progress"></div>
+                            </div>               
+                        </p>
+    					<p>
+                            <div class="progress-label-left"><b>1</b> <i class="fas fa-star text-warning"></i></div>
+                            
+                            <div class="progress-label-right">(<span id="total_one_star_review">0</span>)</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="one_star_progress"></div>
+                            </div>               
+                        </p>
+    				</div>
+    				<div class="col-sm-4 text-center">
+    					<h3 class="mt-4 mb-3">Write Review Here</h3>
+    					<button type="button" name="add_review" id="add_review" class="btn btn-primary">Review</button>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<div class="mt-5" id="review_content"></div>
     </div>
-</header>
-
-<section>
-    <div class="rt-container">
-          <div class="col-rt-12">
-              <div class="Scriptcontent">
-              
-<!-- Student Profile -->
-<div class="student-profile py-4">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="card shadow-sm">
-          <div class="card-header bg-transparent text-center">
-            <img src="Image/s_icon.png" alt="s_icon" style="width:100%">
-            <h3><?php echo $_SESSION['user_id']; ?></h3>
-          </div>
-          <div class="card-body">
-            <?php
-
-			 				echo "<b>";
-
-			 			
-				 				
-
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> STUDENT ID: </b> ";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['STUDENT_ID'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-				 				
-				 		?>
-
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-8">
-        <div class="card shadow-sm">
-          <div class="card-header bg-transparent border-0">
-            <h3 class="mb-0"><i class="far fa-clone pr-1"></i>General Information</h3>
-          </div>
-          <div class="card-body pt-0">
-            <table class="table table-bordered">
-              <?php
-
-			 				echo "<b>";
-			 				echo "<table class='table table-bordered'>";
-				 				
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Name: </b> ";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['NAME'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Email: </b>";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['STUDENT_EMAIL'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Course: </b>";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['COURSE'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-				 				
-
-
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Gender: </b>";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['GENDER'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Adress: </b>";
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['CURRENT_RESIDENCE'];
-				 					echo "</td>";
-				 				echo "</tr>";
-
-				 		
-				 				echo "<tr>";
-				 					echo "<td>";
-				 						echo "<b> Year of study: </b>";	
-				 					echo "</td>";
-				 					echo "<td>";
-				 						echo $row['YEAR_OF_STUDY'];
-				 					echo "</td>";
-				 				echo "</tr>";
-				 				
-				 				
-				 					
-				 				
-			 				echo "</table>";
-			 				echo "</b>";
-			 			?>
-            </table>
-          </div>
-        </div>
-        <div class="row container">
-					<div class="col-md-4 ">
-						<h3><b>Rating & Reviews</b></h3>
-						<div class="row">
-						
-							<div class="col-md-6">
-								<h3 align="center"><b><?php echo round($AVGRATE,1);?></b> <i class="fa fa-star" data-rating="2" style="font-size:20px;color:#ff9f00;"></i></h3>
-								<p><?=$Total;?> ratings and <?=$Total_review;?> reviews</p>
-							</div>
-							<div class="col-md-6">
-								<?php
-								while($db_rating= mysqli_fetch_array($rating)){
-								?>
-									<h4 align="center"><?=$db_rating['rating'];?> <i class="fa fa-star" data-rating="2" style="font-size:20px;color:green;"></i> Total <?=$db_rating['Total'];?></h4>
-									
-									
-								<?php	
-								}
-									
-								?>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">	
-							<?php
-								while($db_review= mysqli_fetch_array($review)){
-							?>
-									<h4><?=$db_review['rating'];?> <i class="fa fa-star" data-rating="2" style="font-size:20px;color:green;"></i> by <span style="font-size:14px;"><?=$db_review['email'];?></span></h4>
-									<p><?=$db_review['remark'];?></p>
-									<hr>
-							<?php	
-								}
-									
-							?>
-							</div>
-						</div>
-							
-						
-						<div id="rating_div">
-									<div class="star-rating">
-										<span class="fa divya fa-star-o" data-rating="1" style="font-size:20px;"></span>
-										<span class="fa fa-star-o" data-rating="2" style="font-size:20px;"></span>
-										<span class="fa fa-star-o" data-rating="3" style="font-size:20px;"></span>
-										<span class="fa fa-star-o" data-rating="4" style="font-size:20px;"></span>
-										<span class="fa fa-star-o" data-rating="5" style="font-size:20px;"></span>
-										<input type="hidden" name="whatever3" class="rating-value" value="1">
-									</div>
-						</div>
-					</div>
-					</div><br>
-					<input type="hidden" name="demo_id" id="demo_id" value="1">
-					<div class="col-md-4">
-				
-					<textarea class="form-control" rows="5" placeholder="Write your review here..." name="remark" id="remark" required></textarea><br>
-					<p><button  class="btn btn-default btn-sm btn-info" id="srr_rating">Submit</button></p>
-					</div>
-					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-					<script src="js/index.js"></script>
-      </div>
-    </div>
-  </div>
-</div>
-
-	</body>
+</body>
 </html>
+
+<div id="review_modal" class="modal" tabindex="-1" role="dialog">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+	      	<div class="modal-header">
+	        	<h5 class="modal-title">Submit Review</h5>
+	        	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          		<span aria-hidden="true">&times;</span>
+	        	</button>
+	      	</div>
+	      	<div class="modal-body">
+	      		<h4 class="text-center mt-2 mb-4">
+	        		<i class="fas fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
+	        	</h4>
+	        	<div class="form-group">
+	        		<input type="text" name="user_name" id="user_name" class="form-control" placeholder="Enter Your Name" />
+	        	</div>
+	        	<div class="form-group">
+	        		<textarea name="user_review" id="user_review" class="form-control" placeholder="Type Review Here"></textarea>
+	        	</div>
+	        	<div class="form-group text-center mt-4">
+	        		<button type="button" class="btn btn-primary" id="save_review">Submit</button>
+	        	</div>
+	      	</div>
+    	</div>
+  	</div>
+</div>
+<script>
+	
+		var rating_data = 0;
+
+    $('#add_review').click(function(){
+
+        $('#review_modal').modal('show');
+
+    });
+
+    $(document).on('mouseenter', '.submit_star', function(){
+
+        var rating = $(this).data('rating');
+
+        reset_background();
+
+        for(var count = 1; count <= rating; count++)
+        {
+
+            $('#submit_star_'+count).addClass('text-warning');
+
+        }
+
+    });
+
+    function reset_background()
+    {
+        for(var count = 1; count <= 5; count++)
+        {
+
+            $('#submit_star_'+count).addClass('star-light');
+
+            $('#submit_star_'+count).removeClass('text-warning');
+
+        }
+    }
+
+    $(document).on('mouseleave', '.submit_star', function(){
+
+        reset_background();
+
+        for(var count = 1; count <= rating_data; count++)
+        {
+
+            $('#submit_star_'+count).removeClass('star-light');
+
+            $('#submit_star_'+count).addClass('text-warning');
+        }
+
+    });
+
+    $(document).on('click', '.submit_star', function(){
+
+        rating_data = $(this).data('rating');
+
+    });
+
+    $('#save_review').click(function(){
+
+        var user_name = $('#user_name').val();
+
+        var user_review = $('#user_review').val();
+
+        if(user_name == '' || user_review == '')
+        {
+            alert("Please Fill Both Field");
+            return false;
+        }
+        else
+        {
+            $.ajax({
+                url:"submit_rating.php",
+                method:"POST",
+                data:{rating_data:rating_data, user_name:user_name, user_review:user_review},
+                success:function(data)
+                {
+                    $('#review_modal').modal('hide');
+
+                    load_rating_data();
+
+                    alert(data);
+                }
+            })
+        }
+
+	});
+	
+
+   
+
+</script>
