e47c7ec9bf48bb5350fe15ccfa772cab0ff9a782
ali01ft <48999769+ali01ft@users.noreply.github.com>
2022-05-12 13:22:31 +0600

    Update submit_rating.php

--- submit_rating.php
+++ submit_rating.php
@@ -4,24 +4,26 @@ $connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
 if(isset($_POST["rating_data"]))
 {
 
-	$data = array(
-		':user_name'		=>	$_POST["user_name"],
-		':rating_question1'		=>	$_POST["rating_question1"],
-		':rating_question2'		=>	$_POST["rating_question2"],
-		':user_rating'		=>	$_POST["rating_data"],
-		':user_review'		=>	$_POST["user_review"],
-		':datetime'			=>	time()
+	
+		$name		=>	$_POST["user_name"],
+		$rate1	=>	$_POST["rating_question1"],
+		$rate2	=>	$_POST["rating_question2"],
+		$ratedata		=>	$_POST["rating_data"],
+		$review		=>	$_POST["user_review"],
+		$time			=>	time()
 	);
 	//(user_name, rating_question1, rating_question2, user_rating, user_review, datetime)
 	//(:user_name, :rating_question1, :rating_question2, :user_rating, :user_review, :datetime)"
 	$query = "
 	INSERT INTO review_table 
 	(user_name, rating_question1, rating_question2, user_rating, user_review, datetime)
-	VALUES (:user_name, :rating_question1, :rating_question2, :user_rating, :user_review, :datetime)";
+	VALUES ($name, $rate1, $rate2, $ratedata, $review, $time)";
 
-	$statement = $connect->prepare($query);
+	 mysqli_query($connect, $query);
 
-	$statement->execute($data);
+	//$statement = $connect->prepare($query);
+
+	//$statement->execute($data);
 
 	echo "Your Review & Rating Successfully Submitted";
 
