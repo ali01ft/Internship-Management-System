4660e6209ab7a7172ef1267045915ae474f0038b
ali01ft <48999769+ali01ft@users.noreply.github.com>
2022-05-12 13:25:56 +0600

    Update submit_rating.php

--- submit_rating.php
+++ submit_rating.php
@@ -5,19 +5,19 @@ if(isset($_POST["rating_data"]))
 {
 
 	
-		$name		=>	$_POST["user_name"],
-		$rate1	=>	$_POST["rating_question1"],
-		$rate2	=>	$_POST["rating_question2"],
-		$ratedata		=>	$_POST["rating_data"],
-		$review		=>	$_POST["user_review"],
-		$time			=>	time()
-	);
+		$name		=	$_POST["user_name"];
+		$rate1	=	$_POST["rating_question1"];
+		$rate2	=	$_POST["rating_question2"];
+		$ratedata		=	$_POST["rating_data"];
+		$review		=	$_POST["user_review"];
+		$date = date("Y/m/d");
+	
 	//(user_name, rating_question1, rating_question2, user_rating, user_review, datetime)
 	//(:user_name, :rating_question1, :rating_question2, :user_rating, :user_review, :datetime)"
 	$query = "
 	INSERT INTO review_table 
 	(user_name, rating_question1, rating_question2, user_rating, user_review, datetime)
-	VALUES ($name, $rate1, $rate2, $ratedata, $review, $time)";
+	VALUES ($name, $rate1, $rate2, $ratedata, $review, $date)";
 
 	 mysqli_query($connect, $query);
 
