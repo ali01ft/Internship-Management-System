84e2748d64aa01fb1e0bd33d3a3c0732452bfc79
Arsalan1007 <93327842+Arsalan1007@users.noreply.github.com>
2022-05-19 14:46:41 +0800

    Adjusted Login Panel

--- login_acess.php
+++ login_acess.php
@@ -5,10 +5,22 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="styles.css" />
 
+<style > 
+
+
+.center{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 25%;
+}
+
+</style>
+
 </head>
 <body>
-<div id="backgroundshell">
-  <img src="image\swinburne.png" alt="swin_logo" class="swinlogocenter">
+<div>
+  <img src="image\swinburne.png" alt="swin_logo" class="center">
   <br>  
 <br> 
 <br> 
