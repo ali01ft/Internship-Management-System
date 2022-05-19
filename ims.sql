5ba8d093e106177f1f452bd7013335c47be76797
ali01ft <48999769+ali01ft@users.noreply.github.com>
2022-05-19 12:29:53 +0600

    Update ims.sql

--- ims.sql
+++ ims.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2022 at 03:55 AM
+-- Generation Time: May 19, 2022 at 07:49 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.11
 
@@ -66,13 +66,47 @@ INSERT INTO `applicants` (`appID`, `STUDENT_ID`, `Job_ID`, `confirmation`, `Proo
 (88, 101220516, 20, 'YES', NULL, 'pending'),
 (89, 101220213, 20, 'YES', NULL, NULL),
 (90, 101223648, 21, NULL, NULL, NULL),
-(91, 101220611, 19, NULL, NULL, NULL),
+(91, 101220611, 19, 'YES', NULL, NULL),
 (92, 101220668, 21, NULL, NULL, NULL),
 (93, 101220668, 20, 'YES', '93_101220668_2022-05-10.pdf', 'Confirmed'),
-(94, 101220668, 19, NULL, NULL, NULL),
+(94, 101220668, 19, 'YES', NULL, NULL),
 (95, 101220699, 21, NULL, NULL, NULL),
 (96, 101220699, 20, 'YES', '96_101220699_2022-05-10.pdf', 'Confirmed'),
-(97, 101220699, 19, NULL, NULL, NULL);
+(97, 101220699, 19, 'YES', NULL, NULL),
+(98, 101220016, 21, 'YES', NULL, 'pending'),
+(99, 101220016, 20, NULL, NULL, NULL),
+(100, 101220016, 22, NULL, NULL, NULL),
+(101, 101220016, 23, 'YES', NULL, NULL),
+(102, 101220016, 24, 'YES', NULL, NULL),
+(103, 101220016, 19, 'YES', '103_101220016_2022-05-18.pdf', 'Confirmed');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `confirmed_details`
+-- (See below for the actual view)
+--
+CREATE TABLE `confirmed_details` (
+`Job_ID` int(255)
+,`REGIS_NO` int(20)
+,`STUDENT_ID` int(255)
+,`NAME` varchar(50)
+,`STUDENT_EMAIL` varchar(100)
+,`COURSE` varchar(50)
+,`YEAR_OF_STUDY` varchar(1)
+,`CV` varchar(30)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email`
+--
+
+CREATE TABLE `email` (
+  `appID` int(255) NOT NULL,
+  `email` varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -161,7 +195,7 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`STUDENT_ID`, `NAME`, `STUDENT_EMAIL`, `COURSE`, `ENROLL`, `SUPERVISOR`, `GENDER`, `CURRENT_RESIDENCE`, `CONTACT_NO`, `YEAR_OF_STUDY`, `PASSWORD`, `CV`, `USERNAME`) VALUES
-(101220016, 'Eric Kau', '101220016@students.swinburne.edu.my', 'Bachelors of Commerce', NULL, 'jim', 'Male', '567 kuchching road', '0172564896', '3', '1234', 'profile101220016.pdf', 'Eric'),
+(101220016, 'Eric Kau', '101220016@students.swinburne.edu.my', 'Bachelors of Commerce', 'Confirmed', 'jim', 'Male', '567 kuchching road', '0172564896', '3', '1234', 'profile101220016.pdf', 'Eric'),
 (101220213, 'Andrew Michael', '101220213@students.swinburne.edu.my', 'Bachelors of Commerce', NULL, 'ali', 'Male', 'TA165, genting highlands', '0172564897', '3', '12345', 'profile101220213.pdf', 'ErgoProxy'),
 (101220516, 'AaL', '101220516@students.swinburne.edu.my', 'Bachelors of Commerce', NULL, NULL, 'Male', '43 street, london', '0176395488', '2', '12345', 'profile101220516.pdf', 'aas'),
 (101220611, 'Ashfaque Ali', '101220611@students.swinburne.edu.my', 'Bachelors of information technology', NULL, NULL, 'Male', 'dhaka bangladesh', '123456789', '3', '1234', 'profile101220611.pdf', 'shag'),
@@ -200,6 +234,39 @@ CREATE TABLE `student_job_details` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `student_review_table`
+--
+
+CREATE TABLE `student_review_table` (
+  `FeedID` int(255) NOT NULL,
+  `STUDENT_ID` int(255) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `rating_question1` varchar(4) DEFAULT NULL,
+  `rating_question2` varchar(4) DEFAULT NULL,
+  `user_rating` int(5) DEFAULT NULL,
+  `user_review` varchar(255) DEFAULT NULL,
+  `datetime` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_review_table`
+--
+
+INSERT INTO `student_review_table` (`FeedID`, `STUDENT_ID`, `user_name`, `rating_question1`, `rating_question2`, `user_rating`, `user_review`, `datetime`) VALUES
+(10, 101220016, 'Eric Kau', 'Yes', 'Yes', 5, 'asdasd', '2022-05-18');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `confirmed_details`
+--
+DROP TABLE IF EXISTS `confirmed_details`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `confirmed_details`  AS SELECT `a`.`Job_ID` AS `Job_ID`, `j`.`REGIS_NO` AS `REGIS_NO`, `s`.`STUDENT_ID` AS `STUDENT_ID`, `s`.`NAME` AS `NAME`, `s`.`STUDENT_EMAIL` AS `STUDENT_EMAIL`, `s`.`COURSE` AS `COURSE`, `s`.`YEAR_OF_STUDY` AS `YEAR_OF_STUDY`, `s`.`CV` AS `CV` FROM (((`student` `s` join `applicants` `a` on(`s`.`STUDENT_ID` = `a`.`STUDENT_ID`)) join `jobs` `j` on(`a`.`Job_ID` = `j`.`Job_ID`)) join `industry` `i` on(`j`.`REGIS_NO` = `i`.`REGIS_NO`)) WHERE `s`.`ENROLL` = 'confirmed''confirmed'  ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `students_nonappliedjobs`
 --
 DROP TABLE IF EXISTS `students_nonappliedjobs`;
@@ -234,6 +301,12 @@ ALTER TABLE `applicants`
   ADD KEY `Job_ID` (`Job_ID`);
 
 --
+-- Indexes for table `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`appID`);
+
+--
 -- Indexes for table `industry`
 --
 ALTER TABLE `industry`
@@ -253,6 +326,13 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`STUDENT_ID`);
 
 --
+-- Indexes for table `student_review_table`
+--
+ALTER TABLE `student_review_table`
+  ADD PRIMARY KEY (`FeedID`),
+  ADD KEY `STUDENT_ID` (`STUDENT_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -260,13 +340,25 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `applicants`
 --
 ALTER TABLE `applicants`
-  MODIFY `appID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+  MODIFY `appID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+
+--
+-- AUTO_INCREMENT for table `email`
+--
+ALTER TABLE `email`
+  MODIFY `appID` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
   MODIFY `Job_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `student_review_table`
+--
+ALTER TABLE `student_review_table`
+  MODIFY `FeedID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -284,6 +376,12 @@ ALTER TABLE `applicants`
 --
 ALTER TABLE `jobs`
   ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`REGIS_NO`) REFERENCES `industry` (`REGIS_NO`);
+
+--
+-- Constraints for table `student_review_table`
+--
+ALTER TABLE `student_review_table`
+  ADD CONSTRAINT `student_review_table_ibfk_1` FOREIGN KEY (`STUDENT_ID`) REFERENCES `student` (`STUDENT_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
