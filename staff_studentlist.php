afc527b41c5af4f867cf14fa2c6f1a2f78f164ec
Arsalan1007 <93327842+Arsalan1007@users.noreply.github.com>
2022-05-23 17:18:42 +0800

    Added alert system to Student listings

--- staff_studentlist.php
+++ staff_studentlist.php
@@ -195,6 +195,54 @@
 
             <div class="container-fluid px-4">
                 <div class="row my-5">
+                    <?php
+                    $connection = mysqli_connect("localhost","root","");
+                    $db = mysqli_select_db($connection, 'ims');
+                    $queryalert = "SELECT * FROM student where ENROLL = 'pending'";
+                    $queryalert_run = mysqli_query($connection, $queryalert);
+
+                     
+                    if($stud_alert = mysqli_num_rows($queryalert_run))
+                        {
+                           
+                            echo '
+                            <div class="col">
+                                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                 <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                  </symbol>
+                                </svg>
+                                <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                  <div>
+                                    You have '.$stud_alert.' students currently awaiting approval!
+                                  </div>
+                                </div>
+                             </div>   
+                            ';
+                        }
+                    else 
+                        {
+                            echo '
+                            <div class="col">
+                                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                  </symbol>
+                                  </svg>
+                                                            
+                                <div class="alert alert-success d-flex align-items-center" role="alert">
+                                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                                  <div>
+                                    You have no outstanding approvals to make!
+                                      </div>
+                                    </div>
+                            </div>        
+
+                            ';
+                        }              
+
+                    ?>
                     <h3 class="fs-4 mb-3">Define your search</h3>
                     <div class="col">
                         
