187b3ac709cbcb6a77cad23cdb79ff286363a372
Arsalan1007 <93327842+Arsalan1007@users.noreply.github.com>
2022-05-27 20:01:35 +0800

    test

--- student_internshiplisting.php
+++ student_internshiplisting.php
@@ -24,6 +24,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="styles.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
     <title>Student Job Lists</title>
 </head>
 
@@ -134,7 +137,20 @@
                 <div class="row my-5">
                     <h3 class="fs-4 mb-3">List of jobs</h3>
                     <div class="col">
+                       <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                                 <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                                                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                                                  </symbol>
+                                                 </svg>
 
+                                                <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                                  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
+                                                  <div>
+                                                    Note : Jobs will be displayed in the student dashboard after applying. 
+
+                                                  </div>
+                                                </div>   
+                    <div data-alerts="alerts"  data-ids="myid" data-fade="3000"></div>
                     <!--fething data module-->
                     <div class="card">
                         <div class="card-body">
@@ -191,6 +207,22 @@
     </div>
 <!-- /#page-content-wrapper -->
     </div>
+
+
+
+<!-- Apply button alert -->
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("Application successful, Please check the dashboard");
+  });
+});
+</script>
+
+
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
