577c0cba35e1cc9b1cf9dc359533de21c300db76
Arsalan1007 <93327842+Arsalan1007@users.noreply.github.com>
2022-05-28 12:30:40 +0800

    Report template

--- industry_report.php
+++ industry_report.php
@@ -25,19 +25,76 @@
 
              
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
 
 
 
-<h1 style="color: green;"> <?php echo "$cid" ?> </h1>
-<p style="text-align: center;">Company Report</p>
-<p>this Company name is $name has complied with swinburne guidelines and has provided our students with working experience.</p>
+<h1 style="color: green;"> <?php echo "Commpan name $cid" ?> </h1>
+<h2 style="text-align: center;">Company Report</h2>
+<p>This Company has complied with swinburne guidelines and has provided our students with working experience. Below are the students this company employed for internships</p>
 
 <table>
-	<tr></tr>
-	<tr></tr>	
-	<tr></tr>		
+  <tr>
+    <th>Student</th>
+    <th>Job ID</th>
+    <th>Position</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
 
-</table>	
+</body>
+</html>
+
 
 
 <?php
